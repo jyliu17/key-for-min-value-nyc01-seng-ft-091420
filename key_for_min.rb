163f860067736660163e02af71327fed2f6045b7
Johnson Liu <jyliu17@gmail.com>
2020-09-02 01:08:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,14 @@
 
 def key_for_min_value(name_hash)
  
- empty_hash = nil
- 
- name_hash.each do |key, value|
-   
- end 
-end
+lowest_value = nil   
+ lowest_key = nil 
+  name_hash.each do |key, value|
+    
+     if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+      
+    end
+  end
+  lowest_key
