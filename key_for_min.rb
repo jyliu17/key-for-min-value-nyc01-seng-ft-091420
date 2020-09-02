a2c7ad06f5de8dd8064509a671f9c0954e3c4281
Johnson Liu <jyliu17@gmail.com>
2020-09-02 00:42:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ 
+ empty_hash = nil
+ 
+ name_hash.each do |key, value|
+   
+   
 end
